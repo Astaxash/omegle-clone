5874bec50250014eb809a8dc4ef9bcc674aa2cb2
Akhil Sharukh <akhilsharukh7254@gmail.com>
2025-05-19 22:31:32 +0530

    updated glitch 1

--- public/script.js
+++ public/script.js
@@ -1,60 +1,84 @@
 const socket = io("https://stranger-chat-gv7k.onrender.com");
-let localStream, peer, isAudio = true, isVideo = true;
+let localStream;
+let remoteStream;
+let peer;
+let isAudio = true;
+let isVideo = true;
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-  .then(stream => {
-    localStream = stream;
-    document.getElementById("localVideo").srcObject = stream;
-    socket.emit("ready");
-  });
+const localVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
 
-socket.on("partner-found", () => {
-  peer = new SimplePeer({ initiator: true, trickle: false, stream: localStream });
-  peer.on("signal", data => socket.emit("signal", data));
-  peer.on("stream", stream => document.getElementById("remoteVideo").srcObject = stream);
-  peer.on("data", data => {
-    showMessage("Stranger: " + data.toString());
-    document.getElementById("notifySound").play();
-  });
+async function initMedia() {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localVideo.srcObject = localStream;
+}
+
+socket.on("connect", () => {
+  socket.emit("join-room");
 });
 
-socket.on("signal", data => peer.signal(data));
-socket.on("partner-left", () => alert("Partner disconnected"));
+socket.on("init-peer", (initiator) => {
+  peer = new SimplePeer({
+    initiator,
+    trickle: false,
+    stream: localStream
+  });
+
+  peer.on("signal", (data) => {
+    socket.emit("signal", data);
+  });
+
+  peer.on("stream", (stream) => {
+    remoteVideo.srcObject = stream;
+  });
+
+  peer.on("error", (err) => console.error("Peer error:", err));
+});
+
+socket.on("signal", (data) => {
+  peer.signal(data);
+});
+
+// Chat handling
+const chatInput = document.getElementById("chatInput");
+const chatBox = document.getElementById("chatBox");
 
 document.getElementById("sendBtn").onclick = () => {
-  const msg = document.getElementById("messageInput").value;
-  if (msg) {
-    peer.send(msg);
-    showMessage("You: " + msg);
-    document.getElementById("messageInput").value = "";
+  const message = chatInput.value;
+  if (message.trim() !== "") {
+    peer.send(message);
+    addMessage("You", message);
+    chatInput.value = "";
   }
 };
 
-function showMessage(msg) {
-  const box = document.getElementById("chatBox");
-  box.innerHTML += `<div>${msg}</div>`;
-  box.scrollTop = box.scrollHeight;
+peer?.on?.("data", (data) => {
+  addMessage("Stranger", data.toString());
+});
+
+function addMessage(sender, message) {
+  const div = document.createElement("div");
+  div.innerHTML = `<strong>${sender}:</strong> ${message}`;
+  chatBox.appendChild(div);
+  chatBox.scrollTop = chatBox.scrollHeight;
+  new Audio("notification.mp3").play(); // Add this file
 }
 
-document.getElementById("muteBtn").onclick = () => {
+// Buttons
+document.getElementById("toggleMic").onclick = () => {
   isAudio = !isAudio;
   localStream.getAudioTracks()[0].enabled = isAudio;
-  document.getElementById("muteBtn").textContent = isAudio ? "Mute" : "Unmute";
+  document.getElementById("toggleMic").innerText = isAudio ? "Mute" : "Unmute";
 };
 
-document.getElementById("videoBtn").onclick = () => {
+document.getElementById("toggleVideo").onclick = () => {
   isVideo = !isVideo;
   localStream.getVideoTracks()[0].enabled = isVideo;
-  document.getElementById("videoBtn").textContent = isVideo ? "Video Off" : "Video On";
+  document.getElementById("toggleVideo").innerText = isVideo ? "Video Off" : "Video On";
 };
 
-document.getElementById("screenBtn").onclick = async () => {
+document.getElementById("shareScreen").onclick = async () => {
   const screenStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-  const videoTrack = screenStream.getVideoTracks()[0];
-  peer.replaceTrack(localStream.getVideoTracks()[0], videoTrack, localStream);
-};
-
-document.getElementById("disconnectBtn").onclick = () => {
-  peer.destroy();
-  location.reload();
+  peer.replaceTrack(localStream.getVideoTracks()[0], screenStream.getVideoTracks()[0], localStream);
+  localVideo.srcObject = screenStream;
 };
--- server.js
+++ server.js
@@ -1,48 +1,43 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const users = [];
+app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, "public")));
+let waitingUser = null;
 
 io.on("connection", (socket) => {
-  let paired = false;
-  for (let u of users) {
-    if (!u.paired) {
-      u.paired = true;
-      socket.paired = true;
-      socket.partner = u.socket;
-      u.socket.partner = socket;
+  socket.on("join-room", () => {
+    if (waitingUser) {
+      socket.partner = waitingUser;
+      waitingUser.partner = socket;
 
-      socket.emit("partner-found");
-      u.socket.emit("partner-found");
-      paired = true;
-      break;
+      socket.emit("init-peer", true); // This one initiates
+      waitingUser.emit("init-peer", false); // This one responds
+
+      waitingUser = null;
+    } else {
+      waitingUser = socket;
     }
-  }
-
-  if (!paired) users.push({ socket, paired: false });
-
-  socket.on("signal", (data) => {
-    if (socket.partner) socket.partner.emit("signal", data);
   });
 
-  socket.on("message", (msg) => {
-    if (socket.partner) socket.partner.emit("message", msg);
+  socket.on("signal", (data) => {
+    if (socket.partner) {
+      socket.partner.emit("signal", data);
+    }
   });
 
   socket.on("disconnect", () => {
-    const index = users.findIndex(u => u.socket === socket);
-    if (index !== -1) users.splice(index, 1);
     if (socket.partner) {
-      socket.partner.emit("partner-left");
       socket.partner.partner = null;
+      socket.partner.emit("partner-left");
+    }
+    if (waitingUser === socket) {
+      waitingUser = null;
     }
   });
 });
